2c001bba097870389a32eac3dba591e7e0fed082
KuzuriX <ramonosx@Migues-MacBook-Pro.local>
2016-04-11 21:28:52 -0600

    first commit

---
+++ src/main/java/com/cenfotec/componentes/bean/PrimerBean.java
@@ -0,0 +1,23 @@
+package com.cenfotec.componentes.bean;
+
+import java.io.Serializable;
+
+public class PrimerBean implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 119804952187402654L;
+	private String name;
+
+	public String getName() {
+		return "Miguel";
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
